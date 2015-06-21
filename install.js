a82b52c5b1581c523284439d8b743459ba8b091f
M.Mykhalyuk <mim@qapint.com>
2015-06-21 23:15:11 +0300

    fix(install.js): change install dir path

--- install.js
+++ install.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
 	path = require('path'),
-	installDir = getDirectories('./child_modules'),
+	installDir = './child_modules',
 	execSync = require('child_process').execSync;
 
 console.log('install directory == ', path.join(__dirname, installDir));
