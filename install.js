7a7c441e14c9f2da563476adc502227456cce689
M.Mykhalyuk <mim@qapint.com>
2015-06-21 23:21:15 +0300

    fix(install.js): execSync -> exec

--- install.js
+++ install.js
@@ -1,10 +1,10 @@
 var fs = require('fs'),
 	path = require('path'),
 	installDir = './child_modules',
-	execSync = require('child_process').execSync;
+	exec = require('child_process').exec;
 
 console.log('install directory == ', path.join(__dirname, installDir));
 
-execSync('npm install', {cwd: path.join(__dirname, installDir)});
+exec('npm install', {cwd: path.join(__dirname, installDir)});
 
 console.log(installDir, 'install done');
