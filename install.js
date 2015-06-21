16a5f456e10627eb95ab6a10512c596ae4afe42a
M.Mykhalyuk <mim@qapint.com>
2015-06-22 00:06:27 +0300

    refactoring

--- install.js
+++ install.js
@@ -1,10 +1,4 @@
-var fs = require('fs'),
-	path = require('path'),
-	installDir = './child_modules',
+var path = require('path'),
 	exec = require('child_process').exec;
 
-console.log('install directory == ', path.join(__dirname, installDir));
-
-exec('npm install', {cwd: path.join(__dirname, installDir)});
-
-console.log(installDir, 'install done');
+exec('npm install', {cwd: path.join(__dirname, './some_modules')});
